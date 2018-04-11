9fce3723bcc3b762e54a9d06f9eda853afe0325c
petra_bodman <petra_bodman@hotmail.com>
2018-04-11 16:42:29 +0200

    added test for see contacts and add contact, fixed error

--- features/support/world.js
+++ features/support/world.js
@@ -15,6 +15,27 @@ class AddressBookWorld {
     this.page = await this.browser.newPage()
     await this.page.goto(HOME_PAGE)
   }
+
+  async closeHomePage() {
+    await this.browser.close()
+  }
+
+  async pageHasTextContent(expectedContent) {
+    console.log(':: ExpectedContent: ' + expectedContent)
+    const pageContent = await this.page.content()
+    console.log(':: PageContent: ' + pageContent)
+    let match = pageContent.match(expectedContent)
+
+    let actualContent = match[0]
+
+    expect(actualContent).to.be.eq(expectedContent)
+  }
+
+  async clickOnAddContactBtn(){
+    const btnSelector = '.add-contact'
+    await this.page.waitForSelector(btnSelector)
+    await this.page.click(btnSelector)
+  }
 }
 
 setWorldConstructor(AddressBookWorld)
