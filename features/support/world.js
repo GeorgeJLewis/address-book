c18fd234d8e0a6bb66e884b009374c7de3a9b3cc
petra_bodman <petra_bodman@hotmail.com>
2018-04-11 14:07:44 +0200

    add steps for landing page and content

--- features/support/world.js
+++ features/support/world.js
@@ -6,8 +6,6 @@ const HOME_PAGE = 'http://localhost:3000'
 
 class AddressBookWorld {
   constructor() {}
-
-
   //open the homepage using puppeteer
   //async/await
   async openHomePage() {
@@ -18,6 +16,16 @@ class AddressBookWorld {
 
   async closeHomePage() {
     await this.browser.close()
+  }
+
+  async pageHasTextContent(expectedContent) {
+    const pageContent = await this.page.content()
+    console.log(pageContent)
+    let match = pageContent.match(expectedContent)
+    console.log(match)
+    let actualContent = match[0]
+
+    expect(actualContent).to.be.eq(expectedContent)
   }
 }
 
