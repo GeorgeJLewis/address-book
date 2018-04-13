f204ba90c6b329dcb5ab3e9c6c33310646922a2a
George_Lewis <georgejlewis@hotmail.com>
2018-04-13 10:04:07 +0200

    fixed issue with local storage. test now passes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
+const storage = window.localStorage
+const renderContacts = () => {
+  const contacts = JSON.parse(storage.getItem('contacts'))
+  let div = document.createElement('div')
+  let body = document.querySelector('body')
+  div.innerHTML('<h1></h1>')
+  body.appendChild(div)
+}
 document.addEventListener('DOMContentLoaded', () => {
+  renderContacts()
   const addContactForm = document.querySelector('.new-contact-form')
 
   addContactForm.addEventListener('submit', event => {
     event.preventDefault()
-    const storage = window.localStorage
 
-    const { name, email, phone, company, notes, twitter } = contactForm.elements
+    const { name, email, phone, company, notes, twitter } = addContactForm.elements
 
     const contact = {
       id: Date.now(),
@@ -16,6 +24,8 @@ document.addEventListener('DOMContentLoaded', () => {
       notes: notes.value,
       twitter: twitter.value,
     }
+
+    console.log(contact)
 
     console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
     storage.setItem('contacts', JSON.stringify([contact]))
