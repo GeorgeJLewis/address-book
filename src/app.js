666aa4126b28ffdff65803fbaa4c4b457f2b5484
George_Lewis <georgejlewis@hotmail.com>
2018-04-13 10:19:12 +0200

    added implemented element in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 const storage = window.localStorage
 const renderContacts = () => {
   const contacts = JSON.parse(storage.getItem('contacts'))
-  let div = document.createElement('div')
-  let body = document.querySelector('body')
-  div.innerHTML('<h1></h1>')
-  body.appendChild(div)
+  if (contacts) {
+    const ul = document.createElement('ul')
+    contacts.forEach(contact => {
+      let li = document.createElement('li')
+      li.innerHTML = `
+      <span>${contact.name}</span> |
+      <span>${contact.email}</span> |
+      <span>${contact.phone}</span>
+      `
+      ul.appendChild(li)
+    })
+    div.appendChild(ul)
+  } else {
+    div.innerHTML = '<p>You have no contacts in your address book</p>'
+  }
+  let div = document.createElement('.contact-list')
 }
 document.addEventListener('DOMContentLoaded', () => {
   renderContacts()
